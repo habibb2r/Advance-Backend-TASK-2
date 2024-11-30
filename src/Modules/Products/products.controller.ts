2e761a2f7cf4f964e58453560329b180be5413fd
habibb2r <habibhk127@gmail.com>
2024-11-30 23:15:53 +0600

    Created Zod Validation For Product

--- src/Modules/Products/products.controller.ts
+++ src/Modules/Products/products.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { ProductServices } from "./products.service";
+import ProductValidation from "./products.validation";
 
 
 
@@ -7,7 +8,8 @@ import { ProductServices } from "./products.service";
 const createProduct = async( req: Request, res: Response)=>{
     try{
         const {product: productData} = req.body;
-        const result = await ProductServices.createProductIntoDB(productData)
+        const ProductValidator = ProductValidation.parse(productData)
+        const result = await ProductServices.createProductIntoDB(ProductValidator)
         res.status(202).json({
             message: "Bike created successfully",
             success: true,
