7a5fd33617fa22dc11e6fa66da66d08bd5b022e5
habibb2r <habibhk127@gmail.com>
2024-11-30 22:17:05 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { ProductRoutes } from './Modules/Products/products.route';
+const app: Application = express();
+
+
+//parser
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/products', ProductRoutes)
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+export default app;
